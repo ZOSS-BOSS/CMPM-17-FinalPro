800e0bbe62f2ed0058013715f598ed27a1ce5e66
ZOSS-BOSS <zanespeiser@gmail.com>
2025-02-28 16:43:41 -0800

    dear god

--- image_augmentation.py
+++ image_augmentation.py
@@ -10,25 +10,22 @@ dataset_folder = "CMPM-17-FirstRepo/archive/Animal Image Dataset" #The dataset t
  
 animal_folders = os.listdir(dataset_folder)
 
-resize_transforms = v2,Compose([
+resize_transforms = v2.Compose([
     v2.ToTensor(),
-    v2.Resize([225,300]),
+    v2.Resize(size = (225,300)),
 ])
 
-augment_transforms - v2.Compose([
-    v2.RandomResizedCrop(size=(225, 300),  antialias=True),
-    v2.RandomHorizontalFlip(p=0.5),
-    v2.ToDtype(torch.float32, scale=True),
-    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+augment_transforms = v2.Compose([
+    v2.RandomResizedCrop(size=(225, 300), scale = (0.5, 1), antialias=True),
+    v2.RandomHorizontalFlip(p=0.3),
+    v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],),
 ])
 random_animal = []
 
-for animal_category in animal_folders:
-    augmented_animals = 0
- 
-    while augmented_animals<(len(os.listdir(animal_category)/2)):
-        random_animal.append(random.choice(os.listdir(animal_category)))
-        augmented_animals += 1
+picture  = Image.open("CMPM-17-FirstRepo/archive/Animal Image Dataset/cow/OIP-_Dx1fsxBCBSXQ_kgAXAxVwHaDP.jpeg")
+picture = resize_transforms(picture)
+plt.imshow(picture)
+plt.show()
 
-transforms = v2.Compose()
+
         
